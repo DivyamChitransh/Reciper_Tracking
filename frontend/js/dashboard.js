e42913c41e538c431e1bc6c5e2ddf7a1ab5ef934
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-04-02 21:42:05 +0530

    Recipe Tracking App

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000"; 
+const API_BASE = "https://reciper-tracking-4.onrender.com"; 
 
 async function searchRecipes() {
     const query = document.getElementById("searchQuery").value;
